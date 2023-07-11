827b7bc1cafa44c79c17316246f5599691e9c45c
Sankalp1BM21AI141 <139086310+Sankalp1BM21AI141@users.noreply.github.com>
2023-07-11 09:17:53 +0530

    Update quicksort.cpp

--- quicksort.cpp
+++ quicksort.cpp
@@ -3,22 +3,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// This function takes last element as pivot,
-// places the pivot element at its correct position
-// in sorted array, and places all smaller to left
-// of pivot and all greater elements to right of pivot
+
 int partition(int arr[], int low, int high)
 {
-	// Choosing the pivot
+	
 	int pivot = arr[high];
 
-	// Index of smaller element and indicates
-	// the right position of pivot found so far
+	
 	int i = (low - 1);
 
 	for (int j = low; j <= high - 1; j++) {
 
-		// If current element is smaller than the pivot
+		
 		if (arr[j] < pivot) {
 
 			// Increment index of smaller element
@@ -30,16 +26,12 @@ int partition(int arr[], int low, int high)
 	return (i + 1);
 }
 
-// The main function that implements QuickSort
-// arr[] --> Array to be sorted,
-// low --> Starting index,
-// high --> Ending index
+
 void quickSort(int arr[], int low, int high)
 {
 	if (low < high) {
 
-		// pi is partitioning index, arr[p]
-		// is now at right place
+		
 		int pi = partition(arr, low, high);
 
 		// Separately sort elements before
@@ -49,7 +41,7 @@ void quickSort(int arr[], int low, int high)
 	}
 }
 
-// Driver Code
+
 int main()
 {
 	int arr[] = { 10, 7, 8, 9, 1, 5 };
