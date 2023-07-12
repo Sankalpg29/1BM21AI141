a649eac030e437d3a7b54f436864de91afd72442
Sankalp1BM21AI141 <139086310+Sankalp1BM21AI141@users.noreply.github.com>
2023-07-12 06:39:12 +0530

    Update MergeSort.cpp

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -3,71 +3,75 @@
 
 class MergeSort {
 public:
-  void sort(std::vector<int>& arr) {
-    mergeSort(arr, 0, arr.size() - 1);
-  }
+  int operations;  // Counter for basic operations
 
-private:
-  void mergeSort(std::vector<int>& arr, int left, int right) {
-    if (left < right) {
-      int middle = left + (right - left) / 2;
-      mergeSort(arr, left, middle);
-      mergeSort(arr, middle + 1, right);
-      merge(arr, left, middle, right);
+  MergeSort() : operations(0) {}
+
+  void merge(std::vector<int>& arr, int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    std::vector<int> L(n1);
+    std::vector<int> R(n2);
+
+    for (int i = 0; i < n1; ++i) {
+      L[i] = arr[left + i];
+      operations++;
     }
-  }
 
-  void merge(std::vector<int>& arr, int left, int middle, int right) {
-    int n1 = middle - left + 1;
-    int n2 = right - middle;
+    for (int j = 0; j < n2; ++j) {
+      R[j] = arr[mid + 1 + j];
+      operations++;
+    }
 
-    std::vector<int> leftArr(n1);
-    std::vector<int> rightArr(n2);
-
-    for (int i = 0; i < n1; i++)
-      leftArr[i] = arr[left + i];
-    for (int j = 0; j < n2; j++)
-      rightArr[j] = arr[middle + 1 + j];
-
-    int i = 0;
-    int j = 0;
-    int k = left;
-
+    int i = 0, j = 0, k = left;
     while (i < n1 && j < n2) {
-      if (leftArr[i] <= rightArr[j]) {
-        arr[k] = leftArr[i];
-        i++;
+      if (L[i] <= R[j]) {
+        arr[k++] = L[i++];
       } else {
-        arr[k] = rightArr[j];
-        j++;
+        arr[k++] = R[j++];
       }
-      k++;
+      operations++;
     }
 
     while (i < n1) {
-      arr[k] = leftArr[i];
-      i++;
-      k++;
+      arr[k++] = L[i++];
+      operations++;
     }
 
     while (j < n2) {
-      arr[k] = rightArr[j];
-      j++;
-      k++;
+      arr[k++] = R[j++];
+      operations++;
     }
+  }
+
+  void mergeSort(std::vector<int>& arr, int left, int right) {
+    if (left < right) {
+      int mid = left + (right - left) / 2;
+      mergeSort(arr, left, mid);
+      mergeSort(arr, mid + 1, right);
+      merge(arr, left, mid, right);
+    }
+  }
+
+  void sort(std::vector<int>& arr) {
+    operations = 0;
+    mergeSort(arr, 0, arr.size() - 1);
   }
 };
 
 int main() {
-  std::vector<int> arr = {9, 5, 1, 8, 3, 6, 2, 7, 4};
-  
+  std::vector<int> arr = {9, 7, 2, 4, 1, 5};
   MergeSort sorter;
   sorter.sort(arr);
 
   std::cout << "Sorted array: ";
-  for (int num : arr)
+  for (int num : arr) {
     std::cout << num << " ";
+  }
   std::cout << std::endl;
+
+  std::cout << "Basic operations executed: " << sorter.operations << std::endl;
 
   return 0;
 }
