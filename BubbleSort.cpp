e9e57ff96d41f42acd5e23a08a3aaef8e9633cd8
Sankalp1BM21AI141 <139086310+Sankalp1BM21AI141@users.noreply.github.com>
2023-07-12 06:49:11 +0530

    Update BubbleSort.cpp

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -5,32 +5,36 @@ class BubbleSort {
 private:
     int* arr;
     int size;
+    int operationCount;
 
 public:
-    BubbleSort(int* array, int length) {
+    BubbleSort(int* array, int n) {
         arr = array;
-        size = length;
+        size = n;
+        operationCount = 0;
     }
 
     void sort() {
-        for (int i = 0; i < size - 1; ++i) {
-            for (int j = 0; j < size - i - 1; ++j) {
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = 0; j < size - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
-                    // Swap elements if they are in the wrong order
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
+                    swap(arr[j], arr[j + 1]);
+                    operationCount += 3; // Increment operation count for each comparison and swap
                 }
+                operationCount += 2; // Increment operation count for each comparison
             }
         }
     }
 
     void display() {
-        cout << "Sorted array: ";
-        for (int i = 0; i < size; ++i) {
+        for (int i = 0; i < size; i++) {
             cout << arr[i] << " ";
         }
         cout << endl;
+    }
+
+    int getOperationCount() {
+        return operationCount;
     }
 };
 
@@ -38,9 +42,13 @@ int main() {
     int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
     int size = sizeof(arr) / sizeof(arr[0]);
 
-    BubbleSort bubble(arr, size);
-    bubble.sort();
-    bubble.display();
+    BubbleSort bs(arr, size);
+    bs.sort();
+
+    cout << "Sorted array: ";
+    bs.display();
+
+    cout << "Number of basic operations executed: " << bs.getOperationCount() << endl;
 
     return 0;
 }
